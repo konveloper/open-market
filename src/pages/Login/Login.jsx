550f76a794f3fa946250a612432a5c4114b876db
konveloper <konveloper@gmail.com>
2024-02-22 01:51:04 +0900

    feat: 로그인 아이디 밸리데이션 기능 구현

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import imgLogo from 'assets/img/logo.svg';
 import Button from 'components/Common/Button/Button';
@@ -12,13 +12,47 @@ import {
 } from './LoginStyle';
 
 function Login() {
+  const [loginForm, setLoginForm] = useState({
+    username: '',
+    password: '',
+    login_type: 'BUYER',
+  });
+  const [usernameErr, setUsernameErr] = useState('');
+  const [usernameIsValid, setUsernameIsValid] = useState(false);
+
+  const inputChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setLoginForm({
+      ...loginForm,
+      [name]: value,
+    });
+  };
+
+  const usernameHandler = () => {
+    if (!loginForm.username) {
+      setUsernameErr('아이디를 추가해주세요 :)');
+      setUsernameIsValid(false);
+    } else {
+      setUsernameErr('');
+      setUsernameIsValid(true);
+    }
+  };
+
   return (
     <ContSection>
       <H2IR>로그인 페이지</H2IR>
       <ImgLogo src={imgLogo} alt='로고 이미지' />
       <ContInputForm>
         <ContUsername>
-          <Input label='아이디' type='text' name='username' max='20' />
+          <Input
+            label='아이디'
+            type='text'
+            name='username'
+            max='20'
+            onChange={inputChangeHandler}
+            onBlur={usernameHandler}
+            message={usernameErr}
+          />
         </ContUsername>
         <div>
           <Input
@@ -27,6 +61,7 @@ function Login() {
             name='password'
             min='8'
             max='20'
+            onChange={inputChangeHandler}
           />
         </div>
         <Button size='m'>로그인</Button>
