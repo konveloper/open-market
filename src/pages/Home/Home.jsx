c1b8a5275ac0256473a0cceedf60e88cb6a42411
konveloper <konveloper@gmail.com>
2024-02-23 18:00:52 +0900

    style: home 페이지 제작

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import NavBar from 'components/Common/NavBar/NavBar';
+import ProductCardLi from 'components/ProductCardLi/ProductCardLi';
+import Footer from 'components/Common/Footer/Footer';
+import { H2IR, ProductCont } from './HomeStyle';
 
 function Home() {
   return (
     <>
-      <p>Home</p>
+      <NavBar />
+      <ProductCont>
+        <H2IR>전체 제품 페이지</H2IR>
+        <ProductCardLi />
+      </ProductCont>
+      <Footer />
     </>
   );
 }
---
+++ src/pages/Home/HomeStyle.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { Cont, IR } from 'styles/GlobalStyles';
+
+export const H2IR = styled.h2`
+  ${IR}
+`;
+
+export const ProductCont = styled.div`
+  ${Cont}
+  width: 1024px;
+  margin: 150px auto;
+  background-color: antiquewhite;
+`;
